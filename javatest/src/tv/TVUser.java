89991cc7a24d8b8eec8f173587e91e9e884775aa
KITRIGAME <KITRIGAME@KITRIGAME-PC>
2017-09-27 10:29:02 +0900

    fork 후 수정

--- javatest/src/tv/TVUser.java
+++ javatest/src/tv/TVUser.java
@@ -1,39 +1,36 @@
 package tv;
 class SamsungTV{
 	public void powerOn(){
-		System.out.println("삼성tv:전원켜다");
+		System.out.println("�궪�꽦tv:�쟾�썝耳쒕떎");
 	}
 	public void powerOff(){
-		System.out.println("삼성tv:전원끄다");
+		System.out.println("�궪�꽦tv:�쟾�썝�걚�떎");
 	}
 	public void volumeUp(){
-		System.out.println("삼성tv:볼륨올리다");
+		System.out.println("�궪�꽦tv:蹂쇰ⅷ�삱由щ떎");
 	}
 	public void volumeDown(){
-		System.out.println("삼성tv:볼륨내리다");
+		System.out.println("�궪�꽦tv:蹂쇰ⅷ�궡由щ떎");
 	}
 }
 class LgTV{
 	public void turnOn(){
-		System.out.println("엘지tv:전원켜다");
+		System.out.println("�뿕吏�tv:�쟾�썝耳쒕떎");
 	}
 	public void turnOff(){
-		System.out.println("엘지tv:전원끄다");
+		System.out.println("�뿕吏�tv:�쟾�썝�걚�떎");
 	}	
 	public void soundUp(){
-		System.out.println("엘지tv:볼륨올리다");
+		System.out.println("�뿕吏�tv:蹂쇰ⅷ�삱由щ떎");
 	}
 	public void soundDown(){
-		System.out.println("엘지tv:볼륨내리다");
+		System.out.println("�뿕吏�tv:蹂쇰ⅷ�궡由щ떎");
 	}	
 }
-// SamsungTV, LgTV 선택권 존재 경우 :
-// TV변경시마다 자바소스 수정 불가피
-// 1개 파일 변경 수정 - 다른 파일 변경 수정 
-//MAIN - FACTORY 시켜서 결과 리턴 
+
 public class TVUser {
 public static void main(String[] args) {
-	System.out.println("github test");
+	System.out.println("github fork test");
 	SamsungTV s1 = new SamsungTV();
 	s1.powerOn();
 	s1.volumeUp();
